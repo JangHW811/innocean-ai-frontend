2fc9f55ed74da582d47c13125a211baf0eae2914
장현웅 <jhw811@gmail.com>
2025-12-02 22:43:24 +0900

    setting menu안되던 문제 수정

--- src/components/main/MainContent.tsx
+++ src/components/main/MainContent.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Settings } from "lucide-react";
 import { useSessionInfo } from "@/apis/sessions";
 import { useSessionStore } from "@/stores/sessionStore";
+import SettingsMenu from "./SettingsMenu";
 import TabSection from "./TabSection";
 import Welcome from "./Welcome";
 
@@ -13,13 +13,7 @@ export default function MainContent() {
   return (
     <main className="flex-1 flex flex-col min-w-0 bg-white">
       {isJobStarted ? <TabSection /> : <Welcome />}
-      <div className="fixed bottom-8 right-8">
-        <button type="button" className="shadow-lg rounded-full">
-          <div className="w-10 h-10 bg-white text-gray-600 rounded-full border border-gray-100 flex items-center justify-center hover:rotate-90 transition-transform duration-300">
-            <Settings className="w-5 h-5 " />
-          </div>
-        </button>
-      </div>
+      <SettingsMenu />
     </main>
   );
 }
