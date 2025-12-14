1da800a5ee3ecedef9fa3a70e50d9d1410753950
jhw <jhw811@gmail.com>
2025-12-14 23:07:30 +0900

    유저 추가

--- src/data/users.ts
+++ src/data/users.ts
@@ -14,6 +14,13 @@ export interface User {
 // admin888   admin888!
 // admin999   admin999!
 // admin111   admin111!
+// Innocean_test1 test1!
+// Innocean_test2 test2!
+// Innocean_test3 test3!
+// Innocean_test4 test4!
+// Innocean_test5 test5!
+// Innocean_test6 test6!
+// Innocean_test7 test7!
 export const USERS: User[] = [
   { id: "admin1", password: "admin1!", name: "관리자1" },
   { id: "admin2", password: "admin2!", name: "관리자2" },
@@ -25,4 +32,24 @@ export const USERS: User[] = [
   { id: "admin888", password: "admin888!", name: "관리자888" },
   { id: "admin999", password: "admin999!", name: "관리자999" },
   { id: "admin111", password: "admin111!", name: "관리자111" },
+  { id: "Innocean_test1", password: "test1!", name: "Innocean_test1" },
+  { id: "Innocean_test2", password: "test2!", name: "Innocean_test2" },
+  { id: "Innocean_test3", password: "test3!", name: "Innocean_test3" },
+  { id: "Innocean_test4", password: "test4!", name: "Innocean_test4" },
+  { id: "Innocean_test5", password: "test5!", name: "Innocean_test5" },
+  { id: "Innocean_test6", password: "test6!", name: "Innocean_test6" },
+  { id: "Innocean_test7", password: "test7!", name: "Innocean_test7" },
+  { id: "Innocean_test8", password: "test8!", name: "Innocean_test8" },
+  { id: "Innocean_test9", password: "test9!", name: "Innocean_test9" },
+  { id: "Innocean_test10", password: "test10!", name: "Innocean_test10" },
+  { id: "Innocean_test11", password: "test11!", name: "Innocean_test11" },
+  { id: "Innocean_test12", password: "test12!", name: "Innocean_test12" },
+  { id: "Innocean_test13", password: "test13!", name: "Innocean_test13" },
+  { id: "Innocean_test14", password: "test14!", name: "Innocean_test14" },
+  { id: "Innocean_test15", password: "test15!", name: "Innocean_test15" },
+  { id: "Innocean_test16", password: "test16!", name: "Innocean_test16" },
+  { id: "Innocean_test17", password: "test17!", name: "Innocean_test17" },
+  { id: "Innocean_test18", password: "test18!", name: "Innocean_test18" },
+  { id: "Innocean_test19", password: "test19!", name: "Innocean_test19" },
+  { id: "Innocean_test20", password: "test20!", name: "Innocean_test20" },
 ];
